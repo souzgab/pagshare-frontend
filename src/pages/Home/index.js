82ddcb437fcd0067035f7fddae33d59ecce7ef61
Lucas Monteiro <lucas.monteiro@bandtec.com.br>
2020-09-23 22:49:18 +0000

    subindo arquitetura e criando alguns componetes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../../components/Button';
+
+class Home extends React.Component{
+    render(){
+        return (
+            <div>
+                <Button text="Login" />
+            </div>
+        );
+    }
+}
+
+export default Home;
