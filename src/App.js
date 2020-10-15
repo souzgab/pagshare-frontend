1c98ef069dad66221beae9193137ca1bef960ace
Lucas Monteiro <lucas.monteiro@bandtec.com.br>
2020-10-15 00:56:18 +0000

    Adicionando e criando alguns componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import Routes from './routes'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends React.Component{
-    render(){
-        return (
-            <Routes />   
-        );
-    }
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+import { NavigationBar } from "./components/NavigationBar";
+import { Layout } from "./components/Layout";
+import { Footer } from "./components/Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </Layout>
+          <Footer />
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
+import styled from "styled-components";
+
+import logoSvg from "../assets/lblLogo.svg";
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #202020;
+  }
+
+  a,
+  .navbar-brand,
+  .navbar-nav .nav-link {
+    color: #bbb;
+
+    &:hover {
+      color: white;
+    }
+  }
+`;
+export const NavigationBar = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Nav.Link>
+        <Link to="/">
+          <img
+            src={logoSvg}
+            width="221"
+            height="51"
+            alt="React Bootstrap logo"
+          />
+        </Link>
+      </Nav.Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/login">Acessar</Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+);
