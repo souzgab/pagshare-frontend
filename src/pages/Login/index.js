4ec6f3177bcdefdd2bc21396a132c1e96305e064
unknown <gs_arruda@outlook.com>
2020-11-09 22:11:02 -0300

    update

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,59 +1,61 @@
 import React from 'react';
-import './styles.css';
-import { Col, Row, Container, InputGroup, FormControl, Form } from "react-bootstrap";
-import { createMuiTheme } from '@material-ui/core/styles';
+import { Col, Row, Container, InputGroup, FormControl, Form, Image } from "react-bootstrap";
 import styled from "styled-components";
-import Background from "../../assets/images/login.jpg";
-import { green } from '@material-ui/core/colors';
 import { NavigationBar } from "../../components/NavigationBar";
+import Blobs from '../../assets/SVG/Blobs.svg'
 
 const Styles = styled.div`
   background-color: #2D2D2D;
   height: 100vh;
+
+  .side-img{
+    width: 100%;
+    height: 100%;
+  }
+
+  .col-1{
+
+  }
+
+  #page-login{
+    
+  }
+
+  .input-main{
+  }
+
+  .input-in{
+    max-height: 250px;
+  }
 `;
 
-const theme = createMuiTheme({
-  palette: {
-    primary: green,
-  },
-  typography: {
-    fontSize: 22,
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  },
-});
-
-// const classes = useStyles();
 export function SignInSide() {
   return <Styles>
     <NavigationBar/>
-      <Container fluid>
+      <Container fluid id="page-login">
           <Row> 
-            <Col xs={true} sm={true} lg={true}>
-            
+            <Col xs={8} sm={true} lg={true} className="col-1">
+              <Image src={Blobs} className="side-img"/>
             </Col>
 
             <Col xs={true} sm={true} lg={true}> 
-            <Form>
-              <Form.Group controlId="formGroupEmail">
-                <Form.Label className={styled}>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-              <Form.Group controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-            </Form>
+              <Form className="input-main">
+                <Form.Group controlId="formGroupEmail">
+                  <Form.Label style={{
+                    color: "white"
+                  }}>
+                    Email address
+                  </Form.Label><Form.Control className="input-in" type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group controlId="formGroupPassword">
+                  <Form.Label style={{
+                    color: "white"
+                  }}>
+                    Password 
+                  </Form.Label><Form.Control className="input-in" type="password" placeholder="Password" />
+                </Form.Group>
+              </Form>
             </Col>
           </Row>
       </Container>
