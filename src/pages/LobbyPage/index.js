ab137c9d80151c12af1f325afafd6e5462bf3798
unknown <gs_arruda@outlook.com>
2020-11-18 17:55:15 -0300

    update lobby components

--- src/pages/LobbyPage/index.js
+++ src/pages/LobbyPage/index.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
+import LobbyBar from './components/LobbyBar'
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import Container from '@material-ui/core/Container';
-import logoSvg from "../../assets/SVG/lblLogo.svg";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-const drawerWidth = 240;
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 const useStyles = makeStyles((theme) => ({
     heroContent: {
@@ -32,161 +18,74 @@ const useStyles = makeStyles((theme) => ({
           : theme.palette.grey[900],
       backgroundSize: 'cover',
       backgroundPosition: 'center',
+      backgroundColor: "darkgray",
+      height: "100vh",
+      display: "flex",
+      width: "100vw",
+      justifyContent:"flex-start",
+      alignItems:"center",
+      direction: "row"
     },
-    root: {
-        display: 'flex',
-      },
-      appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-      },
-      appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-      menuButton: {
-        marginRight: 36,
-      },
-      hide: {
-        display: 'none',
-      },
-      drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-      },
-      drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-      drawerClose: {
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing(7) + 1,
-        [theme.breakpoints.up('sm')]: {
-          width: theme.spacing(9) + 1,
-        },
-      },
-      toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        width: "100%",
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-      },
-      content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-      },
-  }));
+    mainPaper: {
+      padding: theme.spacing(2),
+      margin: 'auto',
+      height: "85vh",
+      width: "100%"
+    },
+    image: {
+      width: 128,
+      height: 128,
+    },
+    img: {
+      margin: 'auto',
+      display: 'block',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      backgroundColor: "primary"
+    }
+  })
+)
 
 const LobbyPage = () => {
     const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-  
-    const handleDrawerOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleDrawerClose = () => {
-      setOpen(false);
-    };
-  
+
     return(
         <React.Fragment>
             <CssBaseline />
-            {/* <Container maxWidth="xg" component="main" style={{ backgroundColor: "darkgray", height: "100vh", width: "100vw" }} className={classes.heroContent}> */}
-                <div className={classes.root}>
-                <CssBaseline />
-                <AppBar
-                    position="fixed"
-                    className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                    })}
-                >
-                    <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, {
-                        [classes.hide]: open,
-                        })}
-                    >
-                        <img
-                            src={logoSvg}
-                            width="110"
-                            height="26"
-                            alt="React Bootstrap logo"
-                        />
-                    </IconButton>
-                    <Typography variant="h6" noWrap>
-                        Mini variant drawer
-                    </Typography>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    variant="permanent"
-                    className={clsx(classes.drawer, {
-                    [classes.drawerOpen]: open,
-                    [classes.drawerClose]: !open,
-                    })}
-                    classes={{
-                    paper: clsx({
-                        [classes.drawerOpen]: open,
-                        [classes.drawerClose]: !open,
-                    }),
-                    }}
-                >
-                    <div className={classes.toolbar}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                    </IconButton>
-                    </div>
-                    <Divider />
-                    <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                    </List>
-                    <Divider />
-                    <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                        </ListItem>
-                    ))}
-                    </List>
-                </Drawer>
-                <main className={classes.content}>
-                    <div className={classes.toolbar} />
-                    <Container style={{ backgroundColor: "red", height: "85vh", width: "100vw" }}>
-                        
-                    </Container>
-                </main>
-                </div>
-            {/* </Container> */}
+            <Container maxWidth="xg" component="main" className={classes.heroContent}>
+              <LobbyBar/>
+                <Grid xs={12}>
+                  <Paper className={classes.mainPaper}>
+                    <Grid container spacing={3}>
+                      <Grid item xs={12}>
+                        <Paper className={classes.paper}>xs=12</Paper>
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <Paper className={classes.paper}>xs=12 sm=6</Paper>
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <Paper className={classes.paper} >xs=12 sm=6</Paper>
+                      </Grid>
+                      <Grid item xs={6} sm={3}>
+                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                      </Grid>
+                      <Grid item xs={6} sm={3}>
+                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                      </Grid>
+                      <Grid item xs={6} sm={3}>
+                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                      </Grid>
+                      <Grid item xs={6} sm={3}>
+                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                </Grid>
+            </Container>
         </React.Fragment>
     )
 }
